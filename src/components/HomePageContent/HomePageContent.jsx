2c12d39f9aca5610b9b3a96e402081e147341074
Kateryna <katepysova97@gmail.com>
2023-04-25 21:27:42 +0200

    Added d&d basic

--- src/components/HomePageContent/HomePageContent.jsx
+++ src/components/HomePageContent/HomePageContent.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Column from "@components/shared/Column/Column.jsx";
 import { DragDropContext } from "react-beautiful-dnd";
 
 function HomePageContent() {
-  const list = ["Item 1", "Item 2", "Item 3"];
+  const [list, setList] = useState(["Item 1", "Item 2", "Item 3"]);
 
-  const onDragEnd = () => null;
+  const onDragEnd = ({ source, destination }) => {
+    // Make sure we have a valid destination
+    if (!destination) return;
+
+    // Make sure we're actually moving the item
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+
+    // Move the item within the list
+    // Start by making a new list without the dragged item
+    const newList = list.filter((_, idx) => idx !== source.index);
+
+    // Then insert the item at the right location
+    newList.splice(destination.index, 0, list[source.index]);
+
+    // Update the list
+    setList(newList);
+  };
 
   return (
     <main className="main">
